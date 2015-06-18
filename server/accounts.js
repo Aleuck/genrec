7b04fafee82dc5d388f698c03726c66c717ee2c2
Alexandre Leuck <aleuck2004@gmail.com>
2015-06-18 01:25:34 -0300

    allow user creation

--- server/accounts.js
+++ server/accounts.js
@@ -6,21 +6,7 @@ Accounts.validateNewUser(function (currentUser) {
   if(Meteor.user().profile.nivel !== Nivel.ADMINISTRADOR) {
     return false;
   } else {
-    //return true;
+    return true;
   }
   throw new Meteor.Error(403, "Username must have at least 3 characters");
 });
-(function () {
-  if(!Meteor.users.findOne()) {
-    Accounts.createUser({
-      username: "admin",
-      email: "admin@genrec",
-      password: "admin",
-      profile: {
-        nome: "Administrador",
-        nivel: 1,
-        prioridade: 1
-      }
-    });
-  }
-})
